21567142b6bd883c1ff3fd21d4d7b217ba9b831a
Veer101002 <virendrasinhbhikhusinh.chauhan@dcmail.ca>
2024-08-02 21:46:37 -0400

    Shape class added
    Shape class added

--- Assignment 4/Part_4/main-finished.js
+++ Assignment 4/Part_4/main-finished.js
@@ -1,4 +1,6 @@
 // set up canvas
+const para = document.querySelector('p');
+let count = 0;
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -18,14 +20,27 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+
+}
+
+
+class Ball extends Shape{
+  
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+
     this.color = color;
     this.size = size;
+    this.exists = true;
+
   }
 
   draw() {
