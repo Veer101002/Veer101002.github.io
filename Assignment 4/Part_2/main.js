acfec5d21b2e673e15917fca10e158a5babb544b
Veer101002 <virendrasinhbhikhusinh.chauhan@dcmail.ca>
2024-08-02 20:32:59 -0400

    Loop through images

--- Assignment 4/Part_2/main.js
+++ Assignment 4/Part_2/main.js
@@ -5,7 +5,8 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['pic1.jpg', pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg];
+
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
   'pic1.jpg' : 'Closeup of a human eye',
   'pic2.jpg' : 'Rock that looks like a wave',
@@ -14,13 +15,17 @@ const alts = {
   'pic5.jpg' : 'Large moth on a leaf'
 }
 
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
